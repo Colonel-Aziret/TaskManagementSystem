8e49a057c61a9682fa5f771cedead002d0a7d54e
Азирет Раманкулов <Aziret5265@gmail.com>
2024-09-18 11:40:07 +0600

    add registration

--- src/main/java/com/example/taskmanagementsystem/service/UserService.java
+++ src/main/java/com/example/taskmanagementsystem/service/UserService.java
@@ -1,9 +1,7 @@
 package com.example.taskmanagementsystem.service;
 
 import com.example.taskmanagementsystem.entity.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public interface UserService {
+    User create (User user);
 }
